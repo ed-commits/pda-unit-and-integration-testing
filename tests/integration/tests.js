65e1d7c77c97ce67783f9f0650b93bca7cf467f2
ed-commits <edward.alexander.cant@gmail.com>
2020-07-19 18:07:44 +0100

    * tests.js: Add a UI integration test to verify that all number buttons update the display of the running total

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('has number buttons that update the display of the running total', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number0')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1234567890')
+  })
+
 });
